075f1d8a993a8a9fe1233974aaf44d7b03aa9473
Titantompa <tohu@irl.se>
2015-04-23 13:11:55 +0200

    Added static definition of Color.White

--- Sema4/Color.cs
+++ Sema4/Color.cs
@@ -4,6 +4,7 @@ namespace Sema4
     {
         public static Color Off = new Color { Red = 0, Green = 0, Blue = 0 };
         public static Color Error = new Color { Red = 255, Green = 0, Blue = 255 };
+        public static Color White = new Color { Red = 255, Green = 255, Blue = 255 };
 
         public byte Red { get; set; }
         public byte Green { get; set; }
